2025a6d810e56f10975e523662e0b509ffb54853
mark <markpackham1@gmail.com>
2021-06-25 20:56:26 +0100

    add components, https://youtu.be/3dHNOWTI7H8?t=1930

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { AboutComponent } from './components/about/about.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    AboutComponent,
+    AddTaskComponent,
+    FooterComponent,
+    TaskItemComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
